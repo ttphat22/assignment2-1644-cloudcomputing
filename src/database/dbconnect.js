cffc3b538fab512849e3522799e343407c3149d8
ttphat22 <0706667675phat@gmail.com>
2021-11-13 22:40:15 +0700

    First commit

--- src/database/dbconnect.js
+++
@@ -1,8 +0,0 @@
-// const mysql = require('mysql2')
-const mysql = require('mysql2');
-
-const client = mysql.createConnection({host:'localhost', user: 'root', database: 'vmo'});
-console.log('connected client');
-
-module.exports = client.promise();
-
